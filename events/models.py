1c9135804729d3aa13909fc69bb84379254812e2
Kara Guarraci <karaguarraci@gmail.com>
2023-12-18 19:26:16 +0000

    delete .env

--- events/models.py
+++ events/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=100)
     date = models.CharField(max_length=50)
-    address = models.CharField(max_length=300)
+    address = models.CharField(max_length=500)
     description = models.TextField(blank=True) 
 
     def __str__(self):
--- training/models.py
+++ training/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Training(models.Model):
     name = models.CharField(max_length=100)
     date = models.CharField(max_length=50)
+    address = models.CharField(max_length=500, blank=True)
     time = models.CharField(max_length=50)
+    
 
     def __str__(self):
         return f"{self.name}"
